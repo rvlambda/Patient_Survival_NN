2891d9e8bd9b581f0535250d891f23689a1f405a
Revi S <rvlambda@gmail.com>
2022-04-02 17:16:12 +0530

    Made changes

--- prediction.py
+++ prediction.py
@@ -13,9 +13,9 @@ def get_prediction(data,model):
     """
     Predict the class of a given data point.
     """
-    result = 0
+    result = 0.0
     result = model.predict(data)
     if result > 0.5:
-        result = 1
+        result = 1.0
     
     return result
